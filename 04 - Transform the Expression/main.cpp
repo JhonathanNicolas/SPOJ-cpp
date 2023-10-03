304ac3e523efa9478c4ab98588ce93c9445cb5b1
Jhonathan Nicolas <jhonathannicolas@gmail.com>
2023-10-02 23:01:46 -0300

    added solution Traform the Expression

--- 04 - Transform the Expression/main.cpp
+++ 04 - Transform the Expression/main.cpp
@@ -15,7 +15,6 @@ Input:
 (a+(b*c))
 ((a+b)*(z+x))
 ((a+t)*((b+(a+c))^(c+d)))
-
 Output:
 abc*+
 ab+zx+*
@@ -31,211 +30,130 @@ using namespace std;
 #define OPERATORS "+-*/^" /* Keep the most priority on the end*/
 #define MAX_EXPRESSIONS_SIZE 400
 
-class Parents
+class Stack
 {
 public:
 
-    uint16_t deepth;
-    std::string raw_logic;
-    std::string resolved_logic;
-
-    Parents()
+    std::vector<std::string> get_stack()
     {
-        deepth = 0;
-        raw_logic = "";
-        resolved_logic = "";
+        return _raw_stack;
     }
 
-    void set_deepth(uint16_t num)
+    void set_stack(std::string stack)
     {
-        deepth = num;
-    }
-    void set_logic(std::string logic)
-    {
-        raw_logic = logic;
-    }
-    uint16_t get_deepth(uint16_t num)
-    {
-        return deepth;
-    }
-    bool get_is_resolved()
-    {
-        return is_resolved;
-    }
+        std::vector<std::string> splited_stack;
 
-    std::string get_operands()
-    {
-        return operands;
-    }
-
-    std::string get_raw_data()
-    {
-        return raw_logic;
-    }
-
-    std::string get_operators()
-    {
-        return operators;
-    }
-
-    std::vector<uint16_t> get_queue()
-    {
-        return queue;
-    }
-
-    bool resolve()
-    {
-        if (raw_logic.at(0) == '\0')
-            return false;
-        else
+        if (stack.size() > 0)
         {
-            _find_operators_and_operands(raw_logic);
-            resolved_logic = operands + operators;
-            is_resolved = true;
-            return true;
+            splited_stack.resize(stack.size());
         }
+
+        for (size_t i = 0; i < splited_stack.size(); i++)
+        {
+            splited_stack[i] = stack[i];
+        }
+
+
+        _raw_stack = splited_stack;
+
+        _raw_stack.insert(_raw_stack.begin(), "(");
+        _raw_stack.push_back(")");
+        _update_stack_size();
     }
 
-    std::string get_solution()
+    std::string resolve()
     {
-        if (is_resolved)
-            return resolved_logic;
-        else
-            return "";
+        _resolve_brackets();
+        return _raw_stack[0];
     }
 
 private:
+    std::vector<std::string> _raw_stack;
 
-    std::vector<uint16_t> queue;
-    std::string operators = "";
-    std::string operands = "";
-    bool is_resolved = false;
+    size_t _stack_size = 0;
 
-    void _find_operators_and_operands(std::string str)
+    void _resolve_brackets()
+    {
+        int start = 0, end = 0, i = 0;
+        std::string temp_str = "";
+
+        while (_is_breackets(_raw_stack))
+        {
+            temp_str = "";
+
+            if (_raw_stack[i] == "(") /* mark the start of brackets*/
+            {
+                start = i;
+            }
+            else if (_raw_stack[i] == ")")
+            {
+                end = i;
+                _resolve(start, end);
+                i = 0;
+                continue;
+            }
+            i++;
+        }
+    }
+
+    void _update_stack_size()
+    {
+        _stack_size = _raw_stack.size();
+    }
+
+    void _resolve(int start, int end)
     {
         std::string _operators = OPERATORS;
-        std::string collections_operators = "";
+        std::vector<std::string> op_str(_operators.size());
+        std::string str = "", operators = "", operands = "";
         size_t inx_operators = _operators.size() - 1;
         bool is_operand = true;
 
-        for (int i = 0; i < str.size(); i++)
+        for (size_t i = 0; i < _operators.size(); i++)
+        {
+            op_str[i] += _operators[i];
+        }
+
+        for (int i = start + 1 ; i < end; i++)
         {
             is_operand = true;
 
             for (int j = inx_operators; j >= 0; j--)
             {
-                if (str[i] == _operators[j])
+                if (_raw_stack[i] == op_str[j])
                 {
-                    operators += str[i];
+                    operators += _raw_stack[i];
                     is_operand = false;
                     break;
                 }
             }
             if (is_operand)
             {
-                operands += str[i];
-                is_operand = true;
+                operands += _raw_stack[i];
             }
         }
+
+        str = operands + operators;
+        _raw_stack.erase(_raw_stack.begin() + start , _raw_stack.begin() + end + 1);
+        _raw_stack.insert(_raw_stack.begin() + start, str);
     }
 
+    bool _is_breackets(std::vector<string> str)
+    {
+        for (size_t i = 0; i < str.size(); i++)
+        {
+            if ((str[i] == "(") || (str[i] == ")"))
+                return true;
+        }
+
+        return false;
+    }
 };
-
-std::vector<Parents> finder_parents(std::string str_logic)
-{
-    size_t size_str_logic = str_logic.size();
-    std::vector<Parents> parents(0);
-    size_t temp_in = 0, temp_out = 0;
-    uint8_t count = 0;
-    size_t i = 0;
-    bool opened_parents = false;
-    bool closed_parents = false;
-    bool detected_interval = false;
-    std::string temp_string = "";
-
-    while( (str_logic.find('(') != -1) || (str_logic.find('(') != -1) )
-    {
-
-        if (str_logic[i] == '(')
-        {
-            temp_in = i;
-            opened_parents = true;
-        }
-
-        else if (str_logic[i] == ')')
-        {
-            if (opened_parents)
-            {
-                temp_out = i;
-                detected_interval = true;
-                opened_parents = false;
-            }
-        }
-
-        if (detected_interval)
-        {
-            parents.resize(parents.size() + 1);
-
-            for (size_t j = temp_in + 1; j < temp_out; j++)
-            {
-                temp_string += str_logic[j];
-
-            }
-            
-            parents[count].set_deepth(count);
-            parents[count].set_logic(temp_string);
-            
-            temp_string = '(' + temp_string + ')';
-            str_logic.replace(temp_in, temp_string.size(), "#" + std::to_string(count));
-            temp_string = "";
-            count++;
-        }
-        if (detected_interval)
-        {
-            i = 0;
-            detected_interval = false;
-        }
-        else
-            i++;
-    }
-
-    return parents;
-
-}
-
-
-std::string brackets2rpn(std::string str)
-{
-    std::vector<Parents> parents;
-    std::string result = "";
-    std::string temp_sol = "";
-    std::string c_deepth = "";
-    size_t loc = 0;
-
-    Parents deepest_parent;
-
-    parents = finder_parents(str);
-    deepest_parent = parents.back();
-    deepest_parent.resolve();
-    result = deepest_parent.get_solution();
-
-    for (size_t i = 0; i < parents.size() -1; i++)
-    {
-        c_deepth = '#' + std::to_string(i);
-        parents[i].resolve();
-        temp_sol = parents[i].get_solution();
-        loc = result.find(c_deepth);
-        result.replace(loc,2, temp_sol);
-    }
-
-    return result;
-
-}
 
 int main()
 {
-
     std::vector<string> str(0);
+    Stack stack;
     int t = 0;
 
     cin >> t;
@@ -250,9 +168,9 @@ int main()
 
     for (int i = 0; i < t; i++)
     {
-        cout << brackets2rpn(str[i]);
+        stack.set_stack(str[i]);
+        cout << stack.resolve() << endl;
     }
-
 
     return 0;
 }
