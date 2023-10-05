f5e053be50d1026bfe45e57fbea07d00b5e68ea3
Jhonathan Nicolas <jhonathannicolas@gmail.com>
2023-10-05 18:28:36 -0300

    fixed one digit bug

--- 05 - The Next Palindrome/main.cpp
+++ 05 - The Next Palindrome/main.cpp
@@ -1,27 +1,3 @@
-/*
-A positive integer is called a palindrome if its representation in the decimal system is the same when read from left to right and from right to left. For a given positive integer K of not more than 1000000 digits, write the value of the smallest palindrome larger than K to output. Numbers are always displayed without leading zeros.
-
-Input
-The first line contains integer t, the number of test cases. Integers K are given in the next t lines.
-
-Output
-For each K, output the smallest palindrome larger than K.
-
-Example
-Input:
-2
-808
-2133
-
-Output:
-818
-2222
-Warning: large Input/Output data, be careful with certain languages
-
-
-*/
-
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -49,19 +25,15 @@ std::string incrementString(const std::string& str)
 std::string get_smallest_palindrome(std::string& str)
 {
     int n = str.size();
-    bool has_leading_zero = false;
     std::string str_temp = "";
 
     str_temp = str;
 
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n -1; )
     {
         if (str_temp[i] == '0')
         {
-            if ((i + 1) == i)
-                break;
             str_temp.erase(0, 1);
-            has_leading_zero = true;
         }
         else
         {
@@ -69,20 +41,22 @@ std::string get_smallest_palindrome(std::string& str)
         }
     }
 
+    n = str_temp.size();
 
     if (n == 1)
     {
         if (str_temp == "9")
             return "11";
         else
-            return to_string( (str[0] - '0') + 1);
+            return to_string((str_temp[0] - '0') + 1);
     }
 
     bool allNines = all_of(str_temp.begin(), str_temp.end(), [](char c) { return c == '9'; });
-    str_temp = "";
 
     if (allNines)
     {
+        str_temp = "";
+
         for (int i = 0; i < str.size(); i++)
         {
             if (str_temp == "")
