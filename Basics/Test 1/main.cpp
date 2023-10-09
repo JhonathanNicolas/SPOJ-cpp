b926cd55aaf533414abb4de03f3ebbfa6fb619bd
Jhonathan Nicolas <jhonathannicolas@gmail.com>
2023-10-09 16:58:55 -0300

    Solved Test 1

---
+++ Basics/Test 1/main.cpp
@@ -0,0 +1,26 @@
+/*Given two natural numbers (both not greater than 200), each number in the separate line, please print the sum of them.
+Example
+Input:
+2
+3
+
+Output:
+5
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    
+    cin >> num1;
+    cin >> num2;
+
+    cout << (num1+num2) << endl;
+
+    return 0;
+}
+
